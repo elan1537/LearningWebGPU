69484bc4f2ef82481520bd7da4fbe88d2ae0ad06
Sehyeon Park <elan1537@gmail.com>
2023-11-17 09:54:36 +0900

    step002

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int, char **)
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    glfwInit();
+    if (!glfwInit())
+    {
+        std::cerr << "Could not initialize GLFW!" << std::endl;
+        return 1;
+    }
+
+    // Use the window
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Could not open window!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    // At the end of the program, destory the window
+    glfwDestroyWindow(window);
 }
