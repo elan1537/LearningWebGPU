b4911b84490eb3a1231d46af19c6c889836406d3
Sehyeon Park <elan1537@gmail.com>
2023-11-17 10:27:05 +0900

    step003: Attach WebGPU Instance

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
 #include <GLFW/glfw3.h>
+#include <webgpu/webgpu.h>
+#include <iostream>
 
 int main(int, char **)
 {
-    glfwInit();
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain = nullptr;
+
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+
+    if (!instance)
+    {
+        std::cerr << "Could not initialize WebGPU!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "WebGPU instance: " << instance << std::endl;
+
     if (!glfwInit())
     {
         std::cerr << "Could not initialize GLFW!" << std::endl;
         return 1;
     }
 
-    // Use the window
     GLFWwindow *window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
     if (!window)
     {
@@ -24,6 +36,9 @@ int main(int, char **)
         glfwPollEvents();
     }
 
-    // At the end of the program, destory the window
+    wgpuInstanceRelease(instance);
     glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
